e550a7af05a850b42a53cd9e9973bd8b7f0eab21
Shujinko53 <shujinko53@yandex.ru>
2020-05-10 17:33:20 +0300

    Adaptation of script after removing button

--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,16 +1,8 @@
 let navMain = document.querySelector('.main-nav');
-let navCloseButton = document.querySelector('.site-list__item-button--close-button');
 let navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 navMain.classList.add('main-nav--closed');
-
-navCloseButton.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--opened')) {
-    navMain.classList.remove('main-nav--opened');
-    navMain.classList.add('main-nav--closed');
-  }
-});
 
 navToggle.addEventListener('click', function() {
   if (navMain.classList.contains('main-nav--closed')) {
